30822b3b0a1eff5bc2f30cbd50b693ca7c0c10e9
mugilanmonish <mugilanmonish@gmail.com>
2024-04-20 12:47:59 +0530

    second commit

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,9 +1,10 @@
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
+
 public class LoginTest {
 	@Test
 	public void login() {
-		Reporter.log("--------------Job1 - Login------------");
+		Reporter.log("--------------Job1 - Login------------", true);
 	}
 }
