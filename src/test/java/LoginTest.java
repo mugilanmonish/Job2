00a61dbc773da2530170e351184ab230798f4434
mugilanmonish <mugilanmonish@gmail.com>
2024-04-20 12:30:34 +0530

    First commit in job2

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,9 @@
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void login() {
+		Reporter.log("--------------Job1 - Login------------");
+	}
+}
