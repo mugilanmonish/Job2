18ba65b6a852dbd221f8c2735a877aa341eba6f1
mugilanmonish <mugilanmonish@gmail.com>
2024-04-21 12:49:31 +0530

    line added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,4 +1,4 @@
-import org.testng.Assert;
+
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -8,5 +8,6 @@ public class LoginTest {
 	public void login() {
 		Reporter.log("--------------Job2 - Login------------", true);
 //		Assert.fail();
+		// new line added
 	}
 }
